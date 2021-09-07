0a30570dc62e919e97a4296732ce4760aab43966
Alisson Perim Honostorio <alissonhonostorio@gmail.com>
2021-09-07 11:40:13 -0300

    feature: adiciona bcrypt
    adiciona a biblioteca bcrypt ao projeto para encriptar a senha passada pelo usuário

--- src/controllers/userController/create.ts
+++ src/controllers/userController/create.ts
@@ -1,13 +1,16 @@
 import { User } from "../../entities/user/User"
 import { IUserServices } from "../../services/interfaces/IUserService/IUserServices"
 import { userModel } from "../../infra/models/Users/userSchema"
+import bcrypt from "bcrypt"
 
 export class CreateUserController {
   constructor(private _service: IUserServices) { }
   async createUser(req, res) {
     try {
       const { firstName, lastName, email, password } = req.body
-      const newUser = new User({ firstName, lastName, email, password })
+      const salt = await bcrypt.genSalt(10)
+      const cryptoPassword = await bcrypt.hash(password, salt)
+      const newUser = new User({ firstName, lastName, email, password: cryptoPassword })
       const userCreated = await this._service.create(newUser, userModel)
       return res.status(201).send(userCreated)
     } catch (error) {
